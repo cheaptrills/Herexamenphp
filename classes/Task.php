d1c2a9fc93f7dd18cb9f3ac6ab47d44350934bf3
cheaptrills <vincevanalphen@yahoo.com>
2019-08-11 17:31:19 +0200

    added "get remaining time"

--- classes/Task.php
+++ classes/Task.php
@@ -8,6 +8,30 @@ class Task implements JsonSerializable {
     private $comment;
     private $id;
     private $listid;
+    private $userid;
+
+/**
+ * Getter for Userid
+ *
+ * @return [type]
+ */
+public function getUserid()
+{
+    return $this->userid;
+}
+
+/**
+ * Setter for Userid
+ * @var [type] userid
+ *
+ * @return self
+ */
+public function setUserid($userid)
+{
+    $this->userid = $userid;
+    return $this;
+}
+
 
 
     public function jsonSerialize(){
@@ -172,6 +196,18 @@ public function setTitle($title)
     {
         $this->listid = $listid;
         return $this;
+    }
+
+    //met hulp van https://stackoverflow.com/questions/7474762/php-get-how-many-days-and-hours-left-from-a-date
+    public function getRemaining(){
+
+        $date=strtotime($this->date);
+        //Calculate difference
+        $diff=$date - time();//time returns current time in seconds
+        $days=floor($diff/(60*60*24));//seconds/minute*minutes/hour*hours/day)
+        $hours=round(($diff-$days*60*60*24)/(60*60));
+        return "$days days $hours hours remaining";
+
     }
 
     
