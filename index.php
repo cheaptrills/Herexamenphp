78eabe88c1dfdcbb091a94398328c6103bfed266
cheaptrills <vincevanalphen@yahoo.com>
2019-08-14 23:43:03 +0200

    dtyling

--- index.php
+++ index.php
@@ -2,22 +2,25 @@
 require_once("autoload/autoload.php");
 $lists = Dallist::getLists();
 ?>
-<p>index omdat het moet van daphne en haar grote armen</p>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vince'es to do </title>
+    <link rel="stylesheet" href="style/index.css">
+    <title>Vince's to do </title>
 </head>
 <body>
-    <a href="addlist.php">add list</a>
-    <?php
-    foreach($lists as $list){
-        echo("<p><a href='list.php?listid={$list->getId()}'>{$list->getTitle()}</a> </p>");
-    }
-    ?>
+<div class="blok">
+<p>Vince's To-Do list application</p>
+    <a href="addlist.php" class="add">add list</a>
+    <p>Current lists</p>
+        <?php
+            foreach($lists as $list){
+                echo("<p><a class='list' href='list.php?listid={$list->getId()}'>{$list->getTitle()}</a> </p>");
+            }
+        ?>
+</div>
 </body>
 </html>
